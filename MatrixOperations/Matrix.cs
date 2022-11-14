8f6d34e1342f0d00c0a800e853d6fc75aa3b25e5
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-11-14 21:56:26 +0300

    task1 complete

---
+++ MatrixOperations/Matrix.cs
@@ -0,0 +1,58 @@
+﻿namespace MatrixOperations
+{
+    public static class Matrix
+    {
+        private static double GenerateDouble(double min, double max)
+        {
+            Random rnd = new Random();
+            double number = rnd.NextDouble();
+            double rndDouble = number * (max - min) + min;
+            return rndDouble;
+        }
+        public static int[,] Fill(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = new Random().Next(0, 10);
+                }
+            }
+            return arr;
+        }
+        public static double[,] Fill(double[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = GenerateDouble(-10, 10);
+                }
+            }
+            return arr;
+        }
+
+        public static void Print(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Print(double[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{Math.Round(arr[i, j], 2)} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+using MatrixOperations;
+
+int[,] arr = new int[3, 3];
+arr = Matrix.Fill(arr);
+
+int[,] SortArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 1; j < arr.GetLength(1); j++)
+        {
+            int temp = arr[i, j];
+            int flag = 0;
+            for (int t = j-1; t >= 0 && flag != 1;)
+            {
+                if (temp < arr[i, t])
+                {
+                    arr[i,t+1] = arr[i,t];
+                    t--;
+                    arr[i,t+1] = temp;
+                }
+                else flag = 1;
+            }
+        }
+    }
+    return arr;
+}
+
+
+Matrix.Print(arr);
+Console.WriteLine();
+Matrix.Print(SortArray(arr));
